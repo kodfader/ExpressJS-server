293859dabad18f5dab7643c558bfef3acd589eb9
Amro Soliman <247kod@gmail.com>
2024-05-29 23:51:41 +0100

    Adds a valuable comment on process.exit()

--- database/db.js
+++ database/db.js
@@ -9,6 +9,10 @@ const connectDB = async () => {
     console.log('MongoDB connected')
   } catch (err) {
     console.error('MongoDB connection error:', err)
+
+    // This ends the process with some failure and will force
+    // the process to exit as quickly as possible even if there are
+    // some asynchronous operations pending that not completed fully
     process.exit(1);
   }
 }
